cc405947fdb79d6fc49c9612f1103f7c43a57832
milosczvmitic <milosczvmitic@gmail.com>
2019-02-18 15:38:51 +0100

    RestService commit

---
+++ GradleSpringRest/src/main/java/org/example/ws/model/Greeting.java
@@ -0,0 +1,58 @@
+
+package org.example.ws.model;
+
+
+
+public class Greeting {
+
+
+
+    private Long id;
+
+
+
+    private String text;
+
+
+
+    public Greeting() {
+
+
+
+    }
+
+
+
+    public Long getId() {
+
+        return id;
+
+    }
+
+
+
+    public void setId(Long id) {
+
+        this.id = id;
+
+    }
+
+
+
+    public String getText() {
+
+        return text;
+
+    }
+
+
+
+    public void setText(String text) {
+
+        this.text = text;
+
+    }
+
+
+
+}
